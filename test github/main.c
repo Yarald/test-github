dfa9f9efc584cf9474348e56e6b11c22401cc231
Yaroslav Dovgopol <yarald@ya.ru>
2017-10-17 22:25:28 +0300

    Initial Commit

---
+++ test github/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test github
+//
+//  Created by Yarik Dovgopol on 17.10.2017.
+//  Copyright © 2017 Yarik Dovgopol. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
